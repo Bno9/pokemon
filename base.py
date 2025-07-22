089f239038330310247aede946ee86f641d0fdea
Bno <Breno_live2005@hotmail.com>
2025-07-21 23:48:14 -0300

    feature/vantagem

--- base.py
+++
@@ -1,9 +0,0 @@
-class Pokemon:
-    def __init__(self, nome, level, tipo, ataques):
-        self.nome = nome
-        self.level = level
-        self.ataques = ataques
-        self.tipo = tipo
-
-    def ataque(self):
-        print("O pokemon atacou")
